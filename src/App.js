cde6066981c7612c1bc9e09f2a432a20b7d857af
jonghyeon <whdgusdldl22@naver.com>
2024-04-11 14:42:34 +0900

    인트로영역 완료

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomeView from './views/HomeView';
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeView from "./views/HomeView";
+import AboutView from "./views/AboutView";
 const App = () => {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<HomeView />}></Route>
-			</Routes>
-		</BrowserRouter>
-	)
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeView />}></Route>
+        <Route path="/about" element={<AboutView />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;

f407469b08c121be388117752aa803d08a17efe0
jonghyeon <whdgusdldl22@naver.com>
2024-04-11 16:24:58 +0900

    연락처영역 완료

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
 
+const contactTitle = {
+  title: "Contact",
+};
+
+const contactText = {
+  link: "mailto:whdgusdldl22@naver.com",
+  title: "mail : whdgusdldl22@naver.com",
+};
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <section id="contact">
+      <div className="contact__inner">
+        <h2 className="contact__title">{contactTitle.title}</h2>
+        <div className="contact__lines top" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+        <div className="contact__text">
+          <div className="text">
+            <div>
+              <a
+                href={contactText.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {contactText.title}
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="contact__lines" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
